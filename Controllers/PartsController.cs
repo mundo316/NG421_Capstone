1779d1a7d7a869f47239fe913c47596d82a451c5
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-02-17 11:17:04 -0600

    remove bad parts migration

--- Controllers/PartsController.cs
+++ Controllers/PartsController.cs
@@ -16,26 +16,26 @@ namespace capstone.Controllers
     public class PartsController : ControllerBase
     {
 
-        [HttpGet]
-        public IEnumerable<Part> Get()
-        {
-            Part[] parts = null;
-            using (var context = new ApplicationDbContext())
-            {
-                parts = context.Parts.ToArray();
-            }
-            return parts;
+        // [HttpGet]
+        // public IEnumerable<Part> Get()
+        // {
+        //     Part[] parts = null;
+        //     using (var context = new ApplicationDbContext())
+        //     {
+        //         parts = context.Parts.ToArray();
+        //     }
+        //     return parts;
 
-        }
-        [HttpPost]
-        public Part Post([FromBody]Part part)
-        {
-            using (var context = new ApplicationDbContext())
-            {
-                context.Parts.Add(part);
-                context.SaveChanges();
-            }
-            return part;
-        }
+        // }
+        // [HttpPost]
+        // public Part Post([FromBody]Part part)
+        // {
+        //     using (var context = new ApplicationDbContext())
+        //     {
+        //         context.Parts.Add(part);
+        //         context.SaveChanges();
+        //     }
+        //     return part;
+        // }
     }
 }
